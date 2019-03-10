f0f3aa269481b1812ae24069e0d256ddfb43f082
Kingman Douglass <kreedd88@gmail.com>
2019-03-09 20:41:19 -0600

    tweaked css, fixed some code, deleted endless commented out code

--- client.js
+++ client.js
@@ -1,17 +1,11 @@
-console.log( 'JS' );
 $( document ).ready(readyNow);
 
 const roster = [];
-//let totalSalary = 0;
-//let clearButton = $('<button id="clearButton">X</button>')
 
 function readyNow() {
     $('#submitButton').on('click', addEmployee);
     $('#employeeList').on('click', '.rendering', '#clearButton', removeEmployee);
 }
-
- //The application should have an input form that collects _employee first name, 
- //last name, ID number, job title, annual salary_.
 
  class Employee{
     constructor(firstName, lastName, idNumber, title, salary) {
@@ -24,22 +18,10 @@ function readyNow() {
  }
 
  function addEmployee() {
-    console.log('in add employee');
     let newEmployee = new Employee ( $( '#firstNameIn').val(), $('#lastNameIn').val(), $('#idNumberIn').val(), $('#titleIn').val(), $('#salaryIn').val() );
     roster.push(newEmployee);
     render();
     clearInputFields();
-    // totalMonthlySalaries();   NOTE:moving these down to render cause its called on both add and delete
-    // ifExceeds();
-    
-
-
-
-    //console.log(newEmployee);
-    
-    //$('#tableBody').append(`<tr><td>${newEmployee.firstName}</td><td>${newEmployee.lastName}</td><td>${newEmployee.idNumber}</td><td>${newEmployee.title}</td><td>${newEmployee.salary}</td><td><button id="clearButton">Clear</button></td></tr>`)
-    
-    //$('#total').remove();
 }
 
  function clearInputFields() {
@@ -49,25 +31,6 @@ function readyNow() {
     $('#titleIn').val('') 
     $('#salaryIn').val('')
  }
-
-//  function totalMonthlySalaries(){
-//     for( i=0; i<roster.length; i++){
-//        totalSalary = totalSalary + Number(roster[i].salary)  
-//      }
-//      return totalSalary = totalSalary / 12;
-// //NOTE: okay so why is my calculation concatenating and not creating the exact math
-// //i.e. through my console log when i run 12 a few times as each salary its should just go
-// //1,2,3,etc but its going 1, 2.08333333, 3.17366666, etc.....
-//  }
-
-// function ifExceeds() {
-//     console.log('totalSalary', totalSalary);
-//     if (totalSalary > 20000){
-//         return $('#monthlySum').css('color', 'red').append(`<span id="total">${(totalSalary).toFixed(2)}</span>`);
-//     } else {
-//         return $('#monthlySum').append(`<span id="total">${(totalSalary).toFixed(2)}</span>`);
-//     }
-// }
 
 function mathTakeTwo() {
     let totalMonthly = 0;
@@ -82,7 +45,6 @@ function mathTakeTwo() {
     }
 }
 
-
 function removeEmployee() {
     console.log('in remove employee');
     let doubleCheck = $(this).data();
@@ -94,40 +56,19 @@ function removeEmployee() {
         }
     }
     render();
-}
-
-//function removeEmployee() {
-        //$(this).remove()
-    //$('#useClear').remove();
-    
-    //$('#tableBody').remove('<span id="useClear">`<tr><td>${newEmployee.firstName}</td><td>${newEmployee.lastName}</td><td>${newEmployee.idNumber}</td><td>${newEmployee.title}</td><td>${newEmployee.salary}</td><td><button id="clearButton">Clear</button></td></tr>`)</span>');
-
-    //$('#tableBody').append('<span id="useClear">`<tr><td>${newEmployee.firstName}</td><td>${newEmployee.lastName}</td><td>${newEmployee.idNumber}</td><td>${newEmployee.title}</td><td>${newEmployee.salary}</td><td><button id="clearButton">Clear</button></td></tr>`)</span>')
-    //$('#tableBody').append('<span id="useClear"></span>');  
-    //let clearData = $(this).data();
-//}
-  
 
 function render() {
-    //totalMonthlySalaries();
-    //ifExceeds();
     mathTakeTwo();
     $('#tableBody').empty();
     for( let employee of roster) {
         let $employee = $(`<tr class='rendering'>
-        <td class="tdStyle">${employee.firstName}</td>
-        <td class="tdStyle">${employee.lastName}</td>
-        <td class="tdStyle">${employee.idNumber}</td>
-        <td class="tdStyle">${employee.title}</td>
-        <td class="tdStyle">${employee.salary}</td>
-        <td class="tdStyle"><button id="clearButton">Clear</button></td></tr>`)
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.idNumber}</td>
+        <td>${employee.title}</td>
+        <td>${employee.salary}</td>
+        <td><button id="clearButton">Delete</button></td></tr>`)
         $employee.data(employee);
         $('#tableBody').append($employee)
     }
 }
-
- //A 'Submit' button should collect the form information, store the information 
- //to calculate monthly costs, append information to the DOM and clear the input 
- //fields. Using the stored information, calculate monthly costs and append this 
- //to the to DOM. If the total monthly cost exceeds $20,000, add a red background 
- //color to the total monthly cost.
